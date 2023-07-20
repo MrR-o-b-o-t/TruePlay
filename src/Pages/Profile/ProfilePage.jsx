ae0d2ac45fd7a00eb7d8004b3df83f4b7e448463
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2023-07-19 19:23:10 -0500

    Update git brach

--- src/Pages/Profile/ProfilePage.jsx
+++ src/Pages/Profile/ProfilePage.jsx
@@ -4,17 +4,34 @@ import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
+import MainModal from "../../Components/Modal/Modal";
+import PasswordModal from "../../Components/PasswordModal/PasswordModal";
 
 import "./ProfilePage.css";
 
 const ProfilePage = () => {
   const [userEmailVerified, setUserEmailVerified] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [showPasswordModal, setShowPasswordModal] = useState(false);
+  const [userChangePassword, setUserChangePassword] = "";
   const [show, setShow] = useState(true);
-  const handleVerifyEmail = () => {
-    if (!userEmailVerified) {
-      setUserEmailVerified(true);
-      console.log("handleVerifyEmail ran");
-    }
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const openPasswordModal = () => {
+    setShowPasswordModal(true);
+  };
+
+  const handleCloseAlert = () => {
+    setShow(false);
+  };
+
+  const handleChangePassword = () => {
+    const storedUserData = localStorage.getItem("user");
+    const userData = JSON.parse(storedUserData);
+    localStorage.setItem(userData.Password, userChangePassword);
   };
 
   return (
@@ -24,11 +41,11 @@ const ProfilePage = () => {
         <Card.Body>
           <h3 className="mb-4">Account</h3>
           <Card.Text>
-            {!userEmailVerified ? (
+            {!userEmailVerified && show ? (
               <Alert
                 className="email__alert"
                 variant="danger"
-                onClose={() => setShow(false)}
+                onClose={handleCloseAlert}
                 dismissible
               >
                 Your email has not been verified
@@ -39,14 +56,19 @@ const ProfilePage = () => {
           </Card.Text>
           <Card.Title>Email Address</Card.Title>
           <Card.Text>
-            Sample.email@gmail.com{" "}
-            {!userEmailVerified ? <p>(Not Verified)</p> : <p>(Verified)</p>}
-            <Button
-              className="float-right main__btn"
-              onClick={handleVerifyEmail}
-            >
-              Verify Email
-            </Button>
+            <div className="d-flex justify-content-between align-items-start">
+              <div>
+                Sample.email@gmail.com{" "}
+                {!userEmailVerified ? (
+                  <span className="text-danger">(Not Verified)</span>
+                ) : (
+                  <span className="text-success">(Verified)</span>
+                )}
+              </div>
+              <Button className="main__btn" onClick={openModal}>
+                Verify Email
+              </Button>
+            </div>
           </Card.Text>
           <Form.Check
             id="newsletterSwitch"
@@ -61,6 +83,11 @@ const ProfilePage = () => {
           >
             Change Password <i className="fas fa-pencil-alt"></i>
           </Button>
+          <PasswordModal
+            show={showPasswordModal}
+            setShow={setShowPasswordModal}
+            handleChangePassword={handleChangePassword}
+          />
         </Card.Footer>
       </Card>
 
@@ -78,6 +105,13 @@ const ProfilePage = () => {
           <Card.Text className="text-muted">June 3, 2023</Card.Text>
         </Card.Footer>
       </Card>
+      <MainModal
+        emailAddress="Sample.email@gmail.com"
+        showModal={showModal}
+        setShowModal={setShowModal}
+        handleCloseAlert={handleCloseAlert}
+        setUserEmailVerified={setUserEmailVerified}
+      />
     </Container>
   );
 };
