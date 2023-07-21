e2ffd8735715258d17318f061ba4cc09f6040a2d
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2023-07-21 14:55:10 -0500

    Update build for Azure deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 // base: / for Azure
 export default defineConfig({
-  base: "/",
+  base: "/dist",
   plugins: [react()],
 });
